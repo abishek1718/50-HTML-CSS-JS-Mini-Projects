d716f7b10e3e08c896c1bd89b2b2ba34dbcf7651
Abishek kanageswaran <kanage01@email.franklin.edu>
2020-12-04 21:13:42 -0500

    Progress steps

--- Project 02/script.js
+++ Project 02/script.js
@@ -1,15 +1,15 @@
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
-const circles = document.querySelectorAll(".circle");
+const circle = document.querySelectorAll(".circle");
 
 let currentActive = 1;
 
 next.addEventListener("click", () => {
   currentActive++;
 
-  if (currentActive > circles.length) {
-    currentActive = circles.length;
+  if (currentActive > circle.length) {
+    currentActive = circle.length;
   }
 
   update();
@@ -26,7 +26,7 @@ prev.addEventListener("click", () => {
 });
 
 function update() {
-  circles.forEach((circle, idx) => {
+  circle.forEach((circle, idx) => {
     if (idx < currentActive) {
       circle.classList.add("active");
     } else {
@@ -37,11 +37,11 @@ function update() {
   const actives = document.querySelectorAll(".active");
 
   progress.style.width =
-    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+    ((actives.length - 1) / (circle.length - 1)) * 100 + "%";
 
   if (currentActive === 1) {
     prev.disabled = true;
-  } else if (currentActive === circles.length) {
+  } else if (currentActive === circle.length) {
     next.disabled = true;
   } else {
     prev.disabled = false;
